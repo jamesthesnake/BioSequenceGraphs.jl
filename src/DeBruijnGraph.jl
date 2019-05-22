fe390e7d0346140d6fa81dbbd5dadd9a77cfa0a3
Arda Akdemir <ardaakdemir1@gmail.com>
2019-05-22 17:51:12 +0900

    Debruijn type initialized

--- src/DeBruijnGraph.jl
+++ src/DeBruijnGraph.jl
@@ -1,12 +1,17 @@
-struct SequenceGraph
+"""
+    DeBruijnGraph is also SequenceGraph with a special design concept
+
+"""
+
+struct DeBruijnGraph
     nodes::Vector{SequenceGraphNode}
     links::Vector{Vector{SequenceGraphLink}}
 end
 
-nodes(sg::SequenceGraph) = sg.nodes
-node(sg::SequenceGraph, i::NodeID) = nodes(sg)[abs(i)]
-links(sg::SequenceGraph) = sg.links
 
+nodes(dbg::DeBruijnGraph) = dbg.nodes
+node(dbg::DeBruijnGraph, i::NodeID) = nodes(dbg)[abs(i)]
+links(dbg::DeBruijnGraph) = dbg.links
 
 
 """
@@ -14,54 +19,18 @@ links(sg::SequenceGraph) = sg.links
 
 Get all of the links of a Node of a sequence graph.
 """
-function links(sg::SequenceGraph, node::NodeID)
-    l = links(sg)
+function links(dbg::DeBruijnGraph, node::NodeID)
+    l = links(dbg)
     @inbounds return l[abs(node)]
 end
 
-
 """
-    add_node!(sg::SequenceGraph, n::SequenceGraphNode)
 
-Add a SequenceGraphNode `n` to a SequenceGraph `sg`.
-
-Returns the node ID used to access the node added from the graph.
+For now this is empty. We have to decide about the design concepts first!!!
 """
-function add_node!(sg::SequenceGraph, n::SequenceGraphNode)
-    newlen = length(push!(nodes(sg),n))
-    resize!(links(sg), newlen)
-    return newlen
+function add_link(dbg::DeBruijnGraph,l::SequenceGraphLink)
+
 end
-
-function remove_node!(sg::SequenceGraph, n::NodeID)
-    oldlinks = copy(links(sg, n))
-    for oldlink in oldlinks
-        remove_link!(sg, source(oldlink), destination(oldlink))
-    end
-end
-
-
-"""
-    add_link!(sg::SequenceGraph, source::NodeID, dest::NodeID, dist::Int)
-
-Construct a link between two nodes in a sequence Graph.
-"""
-function add_link!(sg::SequenceGraph, source::NodeID, dest::NodeID, dist::Int)
-    push!(links(sg, source), Link(source, dest, dist))
-    push!(links(sg, dest), Link(dest, source, dist))
-end
-
-function remove_link!(sg::SequenceGraph, source::NodeID, dest::NodeID)
-    slinks = links(sg, source)
-    slinkslen = length(slinks)
-    filter!(!isequal(SequenceGraphLink(source, dest, 0)), slinks)
-    dlinks = links(sg, dest)
-    dlinkslen = length(dlinks)
-    filter!(!isequal(SequenceGraphLink(dest, source, 0)), dlinks)
-    return slinkslen != length(slinks) || dlinkslen != length(dlinks)
-end
-
-
 
 """
     find_overlaps(X::Vector{SequenceGraphNode})
@@ -87,6 +56,46 @@ function find_overlaps(X::Vector{SequenceGraphNode})
     return overlaps
 end
 
+"""
+
+finds the overlaps between a node and all nodes in a dbg
+"""
+function find_overlaps(dbg::DeBruijnGraph,n::SequenceGraphNode)
+    nodeid = Base.length(dbg.nodes)+1
+    overlaps = Vector{Tuple{Int64,Int64}}()
+    for i in 1:nodeid-1
+        if String(dbg.nodes[i].sequence)[2:end]==String(n.sequence)[1:end-1]
+            push!(overlaps,(i,nodeid))
+            println("Overlap between $(dbg.nodes[i].sequence) and $(n.sequence)")
+        elseif String(dbg.nodes[i].sequence)[1:end-1]==String(n.sequence)[2:end]
+            push!(overlaps,(nodeid,i))
+            println("Overlap between $(dbg.nodes[i].sequence) and $(n.sequence)")
+        end
+    end
+    return overlaps
+end
+
+
+"""
+    add_node(dbg::DeBruijnGraph,n::SequenceGraphNode)
+
+Before adding the node to the graph we find the overlaps between the newly added node and then include it
+Maybe we can consider checking if it already exists 
+For now my plan is to add each corresponding link automatically when we add a node
+"""
+function add_node(dbg::DeBruijnGraph,n::SequenceGraphNode)
+    overlaps  = find_overlaps(dbg,n)
+    push!(dbg.nodes,n)
+    len = Base.length(dbg.nodes)
+    push!(dbg.links,Vector{SequenceGraphLink}()) ## push an empty vector to the links
+    for overlap in overlaps
+        link = SequenceGraphLink(overlap[1],overlap[2])
+        push!(dbg.links[overlap[1]],link)
+        println(link)
+    end
+    nodeid = Base.length(dbg.nodes)
+    nodeid
+end
 
 """
     deBruijn_constructor(kmer_vector::Vector{Kmer{T,K}}) where{T<:NucleicAcid,K}
@@ -137,25 +146,6 @@ function deBruijn_constructor(kmer_vector::Vector{Kmer{T,K}}) where{T<:NucleicAc
     for i in prev_i+1:size(Nodes)[1]
         push!(Links,Vector{SequenceGraphLink}())
     end
-    deBruijn_Graph = SequenceGraph(Nodes,Links)
+    deBruijn_Graph = DeBruijnGraph(Nodes,Links)
     deBruijn_Graph
 end
-
-"""
-    forward_links(sg::SequenceGraph, n::NodeID)
-
-Get the list of links leaving `n`.
-"""
-function forward_links(sg::SequenceGraph, n::NodeID)
-    r = Vector{Link}(0)
-    nodelinks = links(sg, n)
-    sizehint!(r, length(nodelinks))
-    for link in nodelinks
-        if is_forward_link(link, n)
-            push!(r, link)
-        end
-    end
-    return r
-end
-
-backward_links(sg::SequenceGraph, n::NodeID) = forward_links(sg, -n)
